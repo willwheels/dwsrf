b73c4f02c7d0a345b1c7f59f9307bcf6c90a7c5d
willwheels <wheeler.william@epa.gov>
2022-05-05 20:17:23 -0400

    update to read newer files

--- scrape_state_pdfs.R
+++ scrape_state_pdfs.R
@@ -45,37 +45,20 @@ save(state_pdf_links, file = "state_pdf_links.Rda")
 
 download_one_state_pdf <- function(pdf_link) {
   
-  
-  if (pdf_link %in% c("/sites/default/files/2019-12/documents/oklahoma_dwsrf_2019_rpt.pdf",
-                      "/sites/default/files/2019-12/documents/west_virginia_dwsrf_2019_rpt.pdf")
-  ) {
-    
-    filename_use <- stringr::str_replace(pdf_link, "/sites/default/files/2019-12/documents/", "")
-    
-  }
-  
-  else {
-    
-    filename_use <- stringr::str_replace(pdf_link, "/sites/default/files/2020-11/documents/", "")
-    
-  }
-  
-  pdf_base_url <- "https://www.epa.gov"
-  
-  
-  download.file(url = paste0(pdf_base_url, pdf_link),
-                destfile = here::here("state_pdfs", paste0(filename_use)),
+     
+  filename_use <- last(unlist(stringr::str_split(pdf_link, "/")))
+
+  download.file(url = paste0("https://www.epa.gov", pdf_link),
+                destfile = here::here("state_pdfs", filename_use),
                 mode = "wb")
 }
 
 purrr::walk(state_pdf_links$state_pdf_url,download_one_state_pdf)
 
 
-## this works w/ addition of mode = "wb" to download.file
-download_one_state_pdf("/sites/default/files/2020-11/documents/texas_2020.pdf")
 
 
-## verify that each one is 40 pages long
+## verify that each one is 40 pages long--this is no longer true...
 all_state_pdfs <- list.files(here::here("state_pdfs"), full.names = TRUE)
 
 all_state_pdfs_df <- purrr::map_dfr(all_state_pdfs, 
